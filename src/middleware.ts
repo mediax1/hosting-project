71b7e97b86f25a20a037a5647d08f298d5964272
mediax1 <fai3001091@gmail.com>
2026-05-01 16:04:07 +0530

    middleware urls updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")?.value;
+  const rawToken = request.cookies.get("token")?.value;
+  const token = rawToken ? decodeURIComponent(rawToken) : null;
   const { pathname } = request.nextUrl;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
 
   if (pathname.startsWith("/panel") && !token) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/login", baseUrl));
   }
 
   if (pathname === "/login" && token) {
-    return NextResponse.redirect(new URL("/panel", request.url));
+    return NextResponse.redirect(new URL("/panel", baseUrl));
   }
 
   return NextResponse.next();
